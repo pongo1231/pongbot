6678a520a5996553776faf0c60f0ca05cf433f93
Emre Cürgül <pongo1999712@gmail.com>
2019-01-10 21:15:45 +0100

    Version Bump

--- Pongbot/Info.h
+++ Pongbot/Info.h
@@ -7,6 +7,6 @@ namespace Info
 	const char *Description = "A WIP TF2 bot";
 	const char *URL = "None"; // ...yet
 	const char *License = "AGPLv3";
-	const char *Version = "v0.0.6";
+	const char *Version = "v0.0.8";
 	const char *Date = "x";
 }
